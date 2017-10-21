d805d03ac732d19ff875da56200725e6e5c647c5
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2017-10-21 15:40:43 +0530

    Socket eg. files
    Chat client and server files

---
+++ chat/udpServer.py
@@ -0,0 +1,23 @@
+import socket
+
+
+def Main():
+    host = '127.0.0.1'
+    port = 5000
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host, port))
+
+    print "Server Started."
+    while True:
+        data, addr = s.recvfrom(1024)
+        print "message From: " + str(addr)
+        print "from connected user: " + str(data)
+        data = raw_input("=>")
+        print "sending: " + str(data)
+        s.sendto(data, addr)
+    c.close()
+
+
+if __name__ == '__main__':
+    Main()
